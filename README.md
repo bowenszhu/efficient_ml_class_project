81b8cbb7cd9d27e8ba47529a7544e03bf5aac907
Amadou Ngom <amlatyrngom@protonmail.com>
2024-11-25 10:39:58 -0500

    Add basic setup

---
+++ README.md
@@ -0,0 +1,11 @@
+# Setup
+## Basic
+```sh
+git submodule update --init --recursive
+pip install -r requirements.txt
+```
+
+## SmoothQuant -- Opt-1.3B
+```sh
+bash setup.sh opt-1.3b
+```
---
+++ setup.sh
@@ -0,0 +1,24 @@
+smoothquant_path=smoothquant
+# Get model short name from the first argument
+model_short_name=$1
+# Get huggingface model name from the short name
+# Copilot: If startswith opt, then path is facebook/$model_short_name
+# Copilot: If startswith llama, then path is meta-llama/$model_short_name-hf
+if [[ $model_short_name == opt* ]]; then
+    model_full_name="facebook/$model_short_name"
+elif [[ $model_short_name == llama* ]]; then
+    model_full_name="meta-llama/$model_short_name-hf"
+else
+    echo "Model short name must start with opt or llama"
+    exit 1
+fi
+validation_data_url="https://huggingface.co/datasets/mit-han-lab/pile-val-backup/resolve/main/val.jsonl.zst"
+validation_data_path="$smoothquant_path/act_scales/val.jsonl.zst"
+if [ ! -d "$validation_data_path" ]; then
+    wget $validation_data_url -O $validation_data_path
+fi
+
+python $smoothquant_path/examples/generate_act_scales.py \
+    --model-name "$model_full_name" \
+    --output-path "$smoothquant_path/act_scales/$model_short_name.pt" \
+    --dataset-path "$validation_data_path"
